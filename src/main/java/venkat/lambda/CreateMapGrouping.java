f632f1b62f18a0eda0a3d7d330e624ac6e274e04
MarcoBrasci1986 <marco_brasci@hotmail.com>
2016-03-18 16:07:24 +0100

    Adds some examples

---
+++ src/main/java/venkat/lambda/CreateMapGrouping.java
@@ -0,0 +1,39 @@
+package venkat.lambda;
+
+import domain.Gender;
+import domain.Person;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Created by MaBa on 18/03/16.
+ */
+public class CreateMapGrouping {
+
+    public static void main(String[] args) {
+
+        List<Person> people = CreateMap.createPerson();
+
+
+        Map<String, List<Person>> collect = people.stream()
+                .collect(Collectors.groupingBy(Person::getName));
+
+
+        System.out.println(collect);
+
+
+        Map<String, List<Integer>> collectAge = people.stream()
+                .collect(Collectors.groupingBy(Person::getName, // key
+                        Collectors.mapping(Person::getAge, Collectors.toList()))); // value
+
+
+        System.out.println(collectAge);
+
+    }
+
+
+
+}
